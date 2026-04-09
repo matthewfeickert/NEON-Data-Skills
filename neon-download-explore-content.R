0dd193d82ed46c8118b1ad1108578d611e64139e
Matthew Feickert <matthew.feickert@cern.ch>
2026-04-09 01:21:50 -0600

    Add download of data

--- neon-download-explore-content.R
+++ neon-download-explore-content.R
@@ -3,3 +3,18 @@
 library(neonUtilities)
 library(neonOS)
 library(terra)
+
+api_token <- Sys.getenv("NEON_TOKEN")
+
+# To follow the tutorial exactly, download Photosynthetically active
+# radiation (PAR) (DP1.00024.001) data from September-November 2019
+# at Wind River Experimental Forest (WREF).
+data <- loadByProduct(
+  dpID = "DP1.00024.001",
+  site = "WREF",
+  startdate = "2019-09",
+  enddate = "2019-11",
+  check.size = FALSE,
+  nCores = 4,
+  token = api_token
+)
